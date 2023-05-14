278746cb9c323274baa61d124d472bd919899566
PEIWEN-Vivian-Yu <pwy8900@uw.edu>
2023-05-14 10:02:52 -0700

    add youtube video

--- src/components/Home/Card.js
+++ src/components/Home/Card.js
@@ -1,75 +1,3 @@
-
-// //     return(
-// //           <div className="card-info" md={6} lg={4} sm={12}>
-// //             {cardElems}
-// //           </div>
-// // /* <Col className='col-flex' md={6} lg={4} sm={12}>
-// //             <div className='Card-content'>123</div>
-// //         </Col>
-// //         <Col className='col-flex' md={6} lg={4} sm={12}>
-// //             <div className='Card-content'>123</div>
-// //         </Col>
-// //         <Col className='col-flex' md={6} lg={4} sm={12}>
-// //             <div className='Card-content'>123</div>
-// //         </Col>
-// //         <Col className='col-flex' md={6} lg={4} sm={12}>
-// //             <div className='Card-content'>123</div>
-// //         </Col>
-// //         <Col className='col-flex' md={6} lg={4} sm={12}>
-// //             <div className='Card-content'>123</div>
-// //         </Col> */
-// //     )
-// // }
-
-// import React from 'react';
-// import { Square } from './Square.js';
-// import CARDSDATA from '../../data/cards.json';
-// import Container from 'react-bootstrap/Container';
-// import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
-
-// export function Cards(Props) {
-//   const generalpath = "https://github.com/UW-INFO442-SP23/group-heart/tree/main/";
-
-//   const cardElems = CARDSDATA.map((item) => {
-//     return (
-//       <Square
-//         key={item.title}
-//         id={item.id}
-//         title={item.title}
-//         description={item.description}
-//         img_path={item.img_path}
-//         video={item.video}
-//         img_alt={item.img_alt}
-//       />
-//     );
-//   });
-
-//   const splitCards = (arr, size) => {
-//     const result = [];
-//     for (let i = 0; i < arr.length; i += size) {
-//       result.push(arr.slice(i, i + size));
-//     }
-//     return result;
-//   };
-
-//   const rowsOfCards = splitCards(cardElems, 3);
-
-//   return (
-//     <Container>
-//       {rowsOfCards.map((row, index) => (
-//         <Row key={index}>
-//           {row.map((card) => (
-//             <Col className="col-flex" key={card.key}>
-//               {card}
-//             </Col>
-//           ))}
-//         </Row>
-//       ))}
-//     </Container>
-//   );
-// }
-
 import React from 'react';
 import { Square } from './Square.js';
 import CARDSDATA from '../../data/cards.json';
@@ -77,18 +5,16 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-export function Cards(Props) {
-  const generalpath = "https://github.com/UW-INFO442-SP23/group-heart/tree/main/";
+export function Cards() {
+  const generalpath = "https://github.com/UW-INFO442-SP23/group-heart/tree/main";
 
   const cardElems = CARDSDATA.map((item) => {
     return (
       <Square
         key={item.title}
         id={item.id}
-        title={item.title}
-        img_path={item.img_path}
-        video={item.video}
-        img_alt={item.img_alt}
+        title={item.title}i
+        description={item.description}
       />
     );
   });
@@ -117,3 +43,5 @@ export function Cards(Props) {
     </Container>
   );
 }
+
+
--- src/components/Home/Square.js
+++ src/components/Home/Square.js
@@ -1,35 +1,51 @@
-// import React from 'react';
-
-// export function Square(props) {
-//     const generalpath = "https://github.com/UW-INFO442-SP23/group-heart/tree/main/";
-
-//     const image_path = generalpath + props.img_path;
-
-//     return (
-//         <div className="flex-item-card">
-//             <img src={image_path} alt={props.img_alt} />
-//             <div className="card-content" md={6} lg={4} sm={12}>
-//                 <h3 className="project-title">{props.title}</h3>
-//                 <p>{props.description}</p>
-//             </div>
-//         </div>
-
-//     )
-// }
-
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 export function Square(props) {
-  const generalpath = "https://github.com/UW-INFO442-SP23/group-heart/tree/main/";
+  // const generalpath = "https://github.com/UW-INFO442-SP23/group-heart/tree/main/";
 
-  const image_path = generalpath + props.img_path;
+  // const image_path = generalpath + props.img_path;
+
+  const [showPopup, setShowPopup] = useState(false);
+
+  const togglePopup = () => {
+    setShowPopup(!showPopup);
+  };
 
   return (
     <div className="flex-item-card">
-      <img src={image_path} alt={props.img_alt} />
-      <div className="card-content">
-      <h3 className="project-title">{props.title}</h3>
-      </div>
+      <button className="card-button" onClick={togglePopup}>
+        <img src={props.img_path} alt={props.img_alt} />
+        <div className="card-content">
+          <h3 className="project-title">{props.title}</h3>
+        </div>
+      </button>
+      {showPopup && (
+        <div className="popup">
+          <h3>{props.title}</h3>
+          <p>{props.description}</p>
+          <div className="video-container">
+            <iframe
+              width="560"
+              height="315"
+              src={`https://www.youtube.com/embed/${props.video}`}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
+          <button onClick={togglePopup}>Close</button>
+        </div>
+      )}
     </div>
   );
 }
+
+Square.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  video: PropTypes.string.isRequired,
+  img_path: PropTypes.string.isRequired,
+  img_alt: PropTypes.string.isRequired,
+};
