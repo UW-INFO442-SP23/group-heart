3c97e52e94971a3bc5087e013c78f267dd57d028
rhu3 <rhu3@uw.edu>
2023-05-20 22:30:01 -0700

    Update NavBar and new logo

--- src/components/Common/Navbar.js
+++ src/components/Common/Navbar.js
@@ -1,38 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Dropdown from 'react-bootstrap/Dropdown';
-
-
+import React from "react";
+import { Link } from "react-router-dom";
+import Dropdown from "react-bootstrap/Dropdown";
+import img from "../../img/logo1.png";
 export function Navbar(props) {
-
-    return(
-        <div className='spaceBetween-content'>
-        <div className='spaceBetween'>
-            <div>
-            <img className="logo" src="img/logo.png"/>
-            <span>Rainbow</span>
-            </div>
-                <nav>
-                    <div className='links' role="navigation" aria-label="Navigation Bar">
-                        <ul className="links-nav">
-                            <li><Link to="/home" aria-label="Home">Home</Link></li>
-                            <li><Link to="/map/1" aria-label="Map">Map</Link></li>
-                            <li><Link to="/about" aria-label="About Us">About</Link></li>
-                        </ul>
-                    </div>
-                </nav>
-                <Dropdown className='drop-content'>
-                <Dropdown.Toggle className='drop-down' variant="success" id="dropdown-basic">
-                    
-                </Dropdown.Toggle>
-
-                    <Dropdown.Menu>
-                        <Dropdown.Item href="/home">Home</Dropdown.Item>
-                        <Dropdown.Item href="/map/1">Map</Dropdown.Item>
-                        <Dropdown.Item href="/about">About</Dropdown.Item>
-                    </Dropdown.Menu>
-            </Dropdown>
+  return (
+    <div className="spaceBetween-content">
+      <div className="spaceBetween">
+        <div>
+          <img className="logo" src={img} />
+          <span>Rainbow</span>
         </div>
-        </div>
-    )
+        <nav>
+          <div className="links" role="navigation" aria-label="Navigation Bar">
+            <ul className="links-nav">
+              <li>
+                <Link to="/home" aria-label="Home">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/map/1" aria-label="Map">
+                  Map
+                </Link>
+              </li>
+              <li>
+                <Link to="/about" aria-label="About Us">
+                  About
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Dropdown className="drop-content">
+          <Dropdown.Toggle
+            className="drop-down"
+            variant="success"
+            id="dropdown-basic"
+          ></Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="/home">Home</Dropdown.Item>
+            <Dropdown.Item href="/map/1">Map</Dropdown.Item>
+            <Dropdown.Item href="/about">About</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    </div>
+  );
 }
